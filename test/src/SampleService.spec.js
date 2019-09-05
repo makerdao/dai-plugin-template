f987fafdd9a93b881a6ede0583e17842ae050b54
Ethan <ebennett19@gmail.com>
2019-09-05 00:15:34 -0600

    Fix conflicting dependencies

---
+++ test/src/SampleService.spec.js
@@ -0,0 +1,7 @@
+import Maker from '@makerdao/dai';
+import SampleService from '../../src';
+import { testNameToKey } from 'jest-snapshot/build/utils';
+
+test('hello world', () => {
+  console.log('yo');
+});
