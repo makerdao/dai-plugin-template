6e2e06785a74334a0f468e7cb95fdeeac1009d2e
Ethan <ebennett19@gmail.com>
2019-09-06 01:23:48 -0600

    Set up sample test

--- src/SampleService.js
+++ src/SampleService.js
@@ -2,8 +2,19 @@ import { PublicService } from '@makerdao/services-core';
 
 export default class SampleService extends PublicService {
   constructor(name = 'sample') {
-    super(name, ['proxy', 'someOtherService']);
+    super(name, ['proxy', 'token', 'web3']);
   }
 
-  // put yer code here
+  async getUserInfo() {
+    const dai = this.get('token').getToken('DAI');
+    const currentAddress = this.get('web3').currentAddress();
+    const balance = await dai.balanceOf(currentAddress);
+    const proxyAddress = await this.get('proxy').currentProxy();
+
+    return {
+      address: currentAddress,
+      daiBalance: balance,
+      proxyAddress: proxyAddress
+    };
+  }
 }
--- test/src/SampleService.spec.js
+++ test/src/SampleService.spec.js
@@ -1,7 +1,22 @@
 import Maker from '@makerdao/dai';
 import SampleService from '../../src';
-import { testNameToKey } from 'jest-snapshot/build/utils';
 
-test('hello world', () => {
-  console.log('yo');
+async function makerInstance(preset = 'test', network = 'testnet') {
+  const maker = await Maker.create(preset, {
+    plugins: [[SampleService, { network }]],
+    web3: {
+      pollingInterval: 100
+    }
+  });
+  await maker.authenticate();
+  return maker;
+}
+
+test('getUserInfo', async () => {
+  const maker = await makerInstance();
+  const info = await maker.service('sample').getUserInfo();
+
+  expect(info.address).toBe('0x16fb96a5fa0427af0c8f7cf1eb4870231c8154b6');
+  expect(info.daiBalance.toNumber()).toBe(0);
+  expect(info.proxyAddress).toBe('0x570074CCb147ea3dE2E23fB038D4d78324278886');
 });
